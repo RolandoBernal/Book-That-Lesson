0fac1d11884783909476b698dd879a116f82bea3
Rolando Bernal <rolandobernal@Rolandos-MacBook-Pro.local>
2015-06-24 19:10:19 -0500

    Added background

--- js/controllers/userController.js
+++ js/controllers/userController.js
@@ -1,13 +1,5 @@
 app.controller('UserCtrl', ['$scope', function($scope){
 	$scope.userName='Jon Doe';
-	$scope.lessons = [
-		{
-			lesson: {
-									date: '10/10/2015',
-									description: 'driving',
-									instructor: 'tiger woods'
-							}
-		}
 	];
 
 
