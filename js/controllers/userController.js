a59911003b2dc6e49a8031269f51fb9f0049c524
Rolando Bernal <rolandobernal@Rolandos-MacBook-Pro.local>
2015-06-24 20:32:09 -0500

    Just to be safe

--- js/controllers/userController.js
+++ js/controllers/userController.js
@@ -1,5 +1,14 @@
-app.controller('UserCtrl', ['$scope', function($scope){
-	$scope.userName='Jon Doe';
+app.controller('UserCtrl', ['$scope', 'Auth', '$location', function($scope, Auth, $location, $http){
+	$scope.userName= "Levi";
+
+
+	$scope.logout=function(){
+		console.log("You clicked the LOGOUT button");
+		Auth.logout(function() {
+			$location.path('/');
+			$scope.$apply();
+		});
+	};
 
 
 }]);
